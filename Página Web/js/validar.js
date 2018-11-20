d41d95a8ffb2f787f2ec1cec04c54f8b43b6b9cd
ivanmtz20 <43767788+ivanmtz20@users.noreply.github.com>
2018-11-19 18:37:41 -0700

    Add files via upload

--- Página Web/js/validar.js
+++ Página Web/js/validar.js
@@ -1,14 +1,15 @@
 function validar() {
-	var nombre, apellido paterno, apellido materno, fecha, correo, usuario, contraseña, telefono, expresion;
+	var nombre, apellidopaterno, apellidomaterno, correo, usuario, contraseña, telefono, expresion;
     nombre = document.getElementById("nombre").value;
-    apellido paterno = document.getElementById("apellido paterno").value;
-    apellido materno = document.getElementById("apellido materno").value;
+    apellidopaterno = document.getElementById("apellidopaterno").value;
+    apellidomaterno = document.getElementById("apellidomaterno").value;
     fecha = document.getElementById("fecha").value;
     correo = document.getElementById("correo").value;
     usuario = document.getElementById("usuario").value;
     contraseña = document.getElementById("contraseña").value;
     telefono = document.getElementById("telefono").value;
   
+  expresion =  /\w+@\w+\.+[a-z]/;
 
 if(nombre === ""|| apellido paterno === "" || apellido materno === "" 
 || fecha === "" || correo === "" || usuario === "" || contraseña ==="" || telefono === "") {
@@ -21,33 +22,33 @@ else if(nombre.lenght>30) {
 	return false;
 }
 
-else if(apellido paterno.lenght>30 || apellido materno.lenght>30) {
+else if(apellidopaterno.lenght>30 || apellidomaterno.lenght>30) {
 	alert("El apellido es muy largo");
 	return false;
 }
 
-else if(correo.lenght>60) {
-	alert("El correo es muy largo"); 
-	return false; 
-}
-
-else if(fecha.lenght>8) {
-	alert("Fecha invalida"); 
-	return false; 
-}
 
 else if(correo.lenght>50 || usuario.lenght>50) {
 	alert("Demasiado largo"); 
 	return false; 
+} 
+else if(!expresion.test(correo)){
+	alert("El correo no es valido");
+	return false;
 }
 
-else if(contraseña.lenght>20) {
+else if(contraseña.lenght>30) {
 	alert("La contraseña es muy larga"); 
 	return false; 
 }
 
 else if(telefono.lenght>10) {
 	alert("Ingrese numero de 10 digitos"); 
+	return false; 
+}
+
+else if(isNaN(telefono)) {
+	alert("El telefono ingresado no es un número"); 
 	return false; 
 }
 
